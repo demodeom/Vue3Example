a3ba7cb4ecf80e4fb1c77c58d4be6caad0fc6b2e
demodeom <demodeom@example.com>
2024-11-24 14:43:48 +0800

    值: 定义和渲染

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  import {ref} from "vue";
+
+  const num1 = ref();
+  num1.value = 2
+
+  const num2 = ref(0);
+  num2.value = 3
+
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <span>Num1: </span>
+    <span>{{ num1 }}</span>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+
+  <div>
+    <span>Num2: </span>
+    <span>{{ num2 }}</span>
+  </div>
+
+
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
