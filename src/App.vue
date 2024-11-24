ecd92c753522c6f3d06b7b0ff0ef746a3b74bdf1
demodeom <demodeom@example.com>
2024-11-24 22:08:47 +0800

    列表渲染、条件渲染、属性绑定

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,78 @@
 <script setup>
 import {reactive, ref} from "vue";
 
-  // 定义对象
-  const userInfo = reactive({
+// 定义对象
+const userList = reactive([
+  {
+    "id": 1,
     "name": "小甜甜",
     "age": 18,
     "sex": "女",
-  })
-
-  const increaseAge = () => {
-    userInfo.age += Number(changeNum.value);
-  }
-
-  const decreaseAge = () => {
-    userInfo.age -= Number(changeNum.value);
-  }
-
-  const changeNum = ref()
-  changeNum.value = 1
+  },
+  {
+    "id": 2,
+    "name": "小甜甜",
+    "age": 19,
+    "sex": "男",
+  },
+  {
+    "id": 3,
+    "name": "小甜甜",
+    "age": 20,
+    "sex": "女",
+  },
+  {
+    "id": 4,
+    "name": "小甜甜",
+    "age": 21,
+    "sex": "保密",
+  },
+])
 
 </script>
 
 <template>
 
-  <div>
-    <span>姓名: </span>
-    <span>{{ userInfo.name }}</span>
-  </div>
-
-  <div>
-    <span>年龄: </span>
-    <span>{{ userInfo.age }}</span>
-  </div>
-
-  <div>
-    <span>性别: </span>
-    <span>{{ userInfo.sex }}</span>
-  </div>
-
-  <div>
-    <input type="text" v-model="changeNum" />
-  </div>
-
-
-  <div>
-    <button v-on:click="increaseAge">增</button>
-    <button v-on:click="decreaseAge">减</button>
-  </div>
-
-  <div>
-
+  <div class="container">
+    <table>
+      <thead>
+      <tr>
+        <th>学号</th>
+        <th>姓名</th>
+        <th>年龄</th>
+        <th>性别</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(item, index) in userList" :data-index="index">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.age }}</td>
+        <td v-if="item.sex === '男'" style="background-color: blue;color: white">{{ item.sex }}</td>
+        <td v-else-if="item.sex === '女'" style="background-color: red;color: white">{{ item.sex }}</td>
+        <td v-else style="background-color: orange;color: white">{{ item.sex }}</td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 
 </template>
 
 <style scoped>
-div{
-  padding: 10px;
+.container {
+  width: 800px;
+  margin: 0 auto;
 }
 
-button{
-  margin-right: 20px;
+table {
+  width: 800px;
+  border: 1px solid black;
+  border-spacing: 0;
+  text-align: center;
+}
+
+th, td {
+  border: 1px solid black;
+  height: 30px;
 }
 </style>
